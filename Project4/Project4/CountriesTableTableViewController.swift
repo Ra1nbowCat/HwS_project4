d8b092bab7a59cbcd041c3bc1da91712a4db2215
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-04-30 15:01:35 +0300

    Added text to table view

---
+++ Project4/Project4/CountriesTableTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CountriesTableTableViewController.swift
+//  Project4
+//
+//  Created by Илья Лехов on 30.04.2022.
+//
+
+import UIKit
+
+class CountriesTableTableViewController: UITableViewController {
+    
+    var flags = ["estonia", "france", "germany", "ireland", "italy"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        flags.shuffle()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flagsCell", for: indexPath)
+        let flags = flags[indexPath.row]
+        cell.textLabel?.text = flags
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return "All avaliable flags:"
+        }
+}
